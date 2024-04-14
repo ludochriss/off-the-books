30a3921d9f1f43c5dbeeb98716c99be483a86f60
ludochris <ludochriss@gmail.com>
2024-04-14 21:08:54 +1000

    updated url for prod environemnt

--- main/src/environments/environment.prod.ts
+++ main/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cg-apim-021090.azure-api.net/cg-function-app-dev/v1'
+  apiUrl: 'https://cg-apim-1990.azure-api.net/cg-function-app-dev/v1/'
 }
